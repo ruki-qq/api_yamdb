bff858f9a5110d1a0ddcda87294705e7f91ba5bd
Ankor <ankor2023@gmail.com>
2023-11-29 18:48:56 +0300

    Add loaddb script

---
+++ api_yamdb/reviews/management/commands/loaddb.py
@@ -0,0 +1,61 @@
+import csv
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from reviews.models import Category, Genre, Title, GenreTitle, Review, Comment 
+
+class Command(BaseCommand):
+    help = 'Load data from .csv to DB'
+
+    def handle(self, *args, **options):
+        print(options['path'])
+        print(Category.__name__)
+
+        for cl in (Category, Genre, Title, Review, Comment):
+            file = cl.__name__.lower()
+            if file in ('title', 'comment'):
+                file += 's'
+            if file == 'genretitle':
+                file = 'genre_title'
+
+            print(file)
+            with open(f'{options["path"]}/{file}.csv') as f:
+                reader = csv.reader(f)
+                attrs = None
+                for row in reader:
+                    if not attrs:
+                        attrs = row
+                        continue
+
+
+                    a = dict()
+                    for i, attr in enumerate(attrs):
+                        if attr == 'author':
+                            continue;
+                        a[attr] = row[i]
+                        attr_bak = attr
+                        if attr in ('title_id', 'genre_id', 'review_id'):
+                            attr = attr[:-3]
+                        if attr in ('title', 'genre', 'review', 'category'):
+                            attr = attr.capitalize()
+                            #print(attr)
+                            c = globals()[attr];
+                            a[attr.lower()] = c.objects.get(pk=row[i])
+
+
+                    print(a)
+                    t=cl(**a)
+                    t.save()
+
+
+        return "String"
+
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+        '-p',
+        '--path',
+        action='store',
+        default=str(settings.BASE_DIR)+'/static/data',
+        help='Path to .csv files'
+        )
